1165eeecc34606f78c5916b151a34bfaa4f2169f
eandres5 <andresalquinga@hotmail.com>
2020-01-16 16:31:31 -0500

    Version1.2

---
+++ src/models/categoria.ts
@@ -0,0 +1,11 @@
+import { Schema,model } from 'mongoose';
+
+let categoriaSchema = new Schema({
+    nombre :{
+        type: String,
+        required: [true,'Nombre es obligatorio']
+    }
+});
+   
+
+export default model('Categoria',categoriaSchema);
